ea58099259e6bafacc4ceac44af3e02e7399f781
Shermila Guerra <shermilaguerra@gmail.com>
2025-10-21 10:42:57 -0300

    3 do commit

--- src/src/Main_ClasseAstrata.java
+++ src/src/Main_ClasseAstrata.java
@@ -1,16 +1,21 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ *Cálculo de area de poligonos
+* Crie um programa que calcule a area de diferentes figuras geométricas:
+*  Retângulo, TriânguloRetangulo.
+* Use herança para definir uma classe base Poligono e classes filhas para cada figura(Retângulo, TriânguloRetangulo).
+* Use polimorfismo para que o método calcularArea() funcione de forma genérica para todas.
  */
 package src;
 
 public class Main_ClasseAstrata {
 
     public static void main(String[] args) {
-        //Poligono p = new Poligono(2, 5); // apos ter declarado a Classe abstrata chamada "Poligono"(pai das clases)
-        //System.out.println("Área de um poligono: " + p.getArea());// Não consigo mais instanciar a classe poligono
+        //Poligono p = new Poligono(2, 5); // apos ter declarado a Classe "Poligono"(pai das clases)como abstrata eu Não consigo mais instanciar a classe poligono
+        //System.out.println("Área de um poligono: " + p.getArea());// 
         TrianguloRetangulo TR = new TrianguloRetangulo(2, 5);
         System.out.println("Área do TrianguloRetangulo: " + TR.getArea());
+        Retangulo R = new Retangulo(5, 10);
+        System.out.println("Área do Retangulo: " + R.getArea());
     }
     
 }
